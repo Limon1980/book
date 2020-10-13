658dfa6b767f273a14f23f4687d3002f46cc0620
Limon1980 <tarkalmob@yandex.ru>
2020-10-13 17:34:45 +1000

    css bootstrap form -

--- index.php
+++ index.php
@@ -17,8 +17,7 @@ if (!isset($_COOKIE['user_key'])){
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
-		integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/style.css">
 
 	<title>Я не люблю сладкое</title>
